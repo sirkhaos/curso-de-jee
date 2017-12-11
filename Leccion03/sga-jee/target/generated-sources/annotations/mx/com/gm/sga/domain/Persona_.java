c57ea9ab564fbcca0f8fa4b7108bcc32fb45fcbd
alberto torres <alberto.torres.carreno@gmail.com>
2017-12-11 18:57:06 -0300

    commit para comparar con el ejercicion del profesor

--- Leccion03/sga-jee/target/generated-sources/annotations/mx/com/gm/sga/domain/Persona_.java
+++ Leccion03/sga-jee/target/generated-sources/annotations/mx/com/gm/sga/domain/Persona_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-12-04T00:31:51")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-12-11T17:15:24")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
